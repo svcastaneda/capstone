50c1ca29724bb9321bf81f04ca71434e0679347d
svcastaneda <svcastaneda@gmail.com>
2017-04-26 19:25:37 -0500

    Add in some routes to make links work n stuff

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,17 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'course_histories#index'
+  root 'reports#index'
   resources :course_histories
   resources :reports
-  get 'login' => 'users#login', as: :login
-  get 'settings' => 'users#settings', as: :account_settings
+  # get 'login' => 'users#login', as: :login
+  get '/login', to: 'sessions#new', as: :login
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy', as: :logout
+  get '/register', to: 'users#new', as: :new_account
+  post '/register', to: 'users#create', as: :account
+  get '/settings' => 'users#edit'
+  patch '/settings', to: 'users#update'
   
 
   # Example of regular route:
