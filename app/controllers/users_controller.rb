2149711db3e6a2310306848c0c7a1c1900aca70b
svcastaneda <svcastaneda@gmail.com>
2017-04-26 19:24:32 -0500

    Add sessions & user update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,23 @@ class UsersController < ApplicationController
   def new
   end
   
-  def settings
+  def edit
+    current_user ? (@user = current_user) : (redirect_to login_path)
+    # render template: 'users/edit'
   end
   
-  def edit
+  def update
+    @user = current_user
+    if @user.update_attributes(user_params)
+      redirect_to root_path
+    else
+      @errors = @user.errors.full_messages
+      render template: 'users/edit'
+    end
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :username, :password, :password_confirmation)
   end
 end
