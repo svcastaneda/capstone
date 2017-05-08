6f4d206de9cda94506469139f4b91f6ab79b3e26
svcastaneda <svcastaneda@gmail.com>
2017-05-08 11:12:11 -0500

    Make users polymorphic

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ActiveRecord::Base
   has_secure_password
+  
+  belongs_to :account, polymorphic: true
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true
+  validates :fname, presence: true
+  validates :lname, presence: true
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, allow_nil: true, length: {minimum: 8, maximum: 25}
