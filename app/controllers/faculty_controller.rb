ee80cd2b1c0bedb2f54d814fc903f4b59578202b
svcastaneda <svcastaneda@gmail.com>
2017-05-24 17:56:41 -0500

    Add controllers for students & faculty for admin shit

---
+++ app/controllers/faculty_controller.rb
@@ -0,0 +1,48 @@
+class FacultyController < UsersController
+  def index
+    if current_user && current_user.admin?
+      @users = User.where(account_type: 'Faculty')
+      render template: 'users/index'
+    else
+      redirect_to root_path
+    end
+  end
+  #
+  # def show
+  #   if current_user && !current_user.student?
+  #     @user = Student.find_by_id(params[:student_id]).user
+  #   end
+  # end
+  
+  def show
+    if current_user && current_user.admin?
+      @user = User.find_by_id(params[:id])
+    end
+  end
+  
+  def edit
+    redirect_to login_path if !current_user
+    @user = params[:id] ? User.find(params[:id]) : current_user
+    @student = Student.find_by_id(@user.id)
+  end
+  
+  def update
+    @user = params[:id] ? User.find(params[:id]) : current_user
+    if @user && @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to edit_faculty_path(@user)
+    else
+      @errors = @user.errors.full_messages
+      render template: 'users/edit'
+    end
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:fname, :lname, :id, :email, :username, :password, :password_confirmation, :account_type)
+  end
+  
+  def student_params
+    params.require(:student).permit(:degree_id, :advisor_id)
+  end
+end
