057ec226edb105ecb5267556d6ff5bbc5b490d01
svcastaneda <svcastaneda@gmail.com>
2017-05-24 18:01:47 -0500

    Add in s'more routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,12 @@ Rails.application.routes.draw do
   root 'users#index'
   resources :course_histories
   resources :reports
-  # resources :students do
-    # resources :reports
-  # end
+  resources :students do
+    resources :reports
+  end
+  resources :faculty
+  resources :degrees
+  resources :courses
   
   get '/reports/:id', to: 'reports#show', as: :show_report
   get '/login', to: 'sessions#new', as: :login
@@ -16,11 +19,14 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy', as: :logout
   get '/register', to: 'users#new', as: :new_account
   post '/register', to: 'users#create'
-  get '/settings' => 'users#edit'
-  patch '/settings', to: 'users#update'
-  get '/users/:id', to: 'users#show', as: :user
-  get '/faculty', to: 'users#faculty', as: :faculty_index
-  get '/students', to: 'users#students', as: :students_index
+  get '/settings' => 'users#edit', as: :settings
+  patch '/users/:id', to: 'users#update', as: :user
+  delete '/users/:id', to: 'users#destroy'
+  
+  # get '/faculty', to: 'faculty#index', as: :faculty
+  # get '/faculty/:id', to: 'faculty#show', as: :user
+  # get '/students', to: 'users#students', as: :students_index
+  get '/users', to: 'users#all_users', as: :users
   # get '/students/:student_id', to: 'users#show', as: :student
   # get '/students/:student_id/reports', to: 'reports#index', as: :student_reports
   # get '/students/:student_id/reports/:report_id', to: 'reports#show', as: :student_report
