3acb9fb8c79b4ae437ef652beeace07df50d2c11
svcastaneda <svcastaneda@gmail.com>
2017-05-31 19:23:28 -0500

    Fix merge conflict

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,34 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'users#index'
+  resources :course_histories
+  resources :reports
+  resources :students do
+    resources :reports
+  end
+  resources :faculty
+  resources :degrees
+  resources :courses
+  
+  get '/reports/:id', to: 'reports#show', as: :show_report
+  get '/login', to: 'sessions#new', as: :login
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy', as: :logout
+  get '/register', to: 'users#new', as: :new_account
+  post '/register', to: 'users#create'
+  get '/settings' => 'users#edit', as: :settings
+  patch '/users/:id', to: 'users#update', as: :user
+  delete '/users/:id', to: 'users#destroy'
+  
+  # get '/faculty', to: 'faculty#index', as: :faculty
+  # get '/faculty/:id', to: 'faculty#show', as: :user
+  # get '/students', to: 'users#students', as: :students_index
+  get '/users', to: 'users#all_users', as: :users
+  # get '/students/:student_id', to: 'users#show', as: :student
+  # get '/students/:student_id/reports', to: 'reports#index', as: :student_reports
+  # get '/students/:student_id/reports/:report_id', to: 'reports#show', as: :student_report
+  
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
